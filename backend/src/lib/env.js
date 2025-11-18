d9c8ddc012e647ed9eaa576e12a17092812c4589
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-11-18 18:56:03 +0800

    feat: ready for deployment

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -2,4 +2,5 @@ import "dotenv/config";
 
 export const ENV = {
     PORT: process.env.PORT || 3000,
+    NODE_ENV: process.env.NODE_ENV
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,14 +3,26 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { ENV } from "./lib/env.js";
 import AuthRoutes from "./routes/AuthRoutes.route.js";
+import path from "path";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
+const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
 
 app.use("/api/auth", AuthRoutes);
+
+if (ENV.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get((_, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     console.log("Server is running on port", PORT)
