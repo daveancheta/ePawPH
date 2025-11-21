bde178328a4688325f3c7279535d3c2c52161f1d
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-11-21 11:20:54 +0800

    fix: correct PageLoader filename for Vercel build

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,8 @@ import Login from "./pages/auth/Login.js";
 import SignUp from "./pages/auth/SignUp.js";
 import { useEffect } from "react";
 import { UseAuthStore } from "./store/UseAuthStore.ts"
-import PageLoader from "@/components/Pageloader.tsx";
 import { Toaster } from "react-hot-toast"
+import PageLoader from "./components/Pageloader";
 
 function App() {
   const { checkAuth, authUser, isCheckingAuth } = UseAuthStore();
@@ -14,20 +14,19 @@ function App() {
     checkAuth()
   }, [checkAuth])
 
-  if(isCheckingAuth) return <PageLoader/>
-
+  if (isCheckingAuth) return <PageLoader />
 
   return (
     <div className="">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={authUser ? <Home/> : <Login/>}></Route>
-          <Route path='/login' element={authUser ? <Home /> : <Login/>}></Route>
-          <Route path='/signup' element={authUser ? <Home/> : <SignUp />}></Route>
+          <Route path='/' element={authUser ? <Home /> : <Login />}></Route>
+          <Route path='/login' element={authUser ? <Home /> : <Login />}></Route>
+          <Route path='/signup' element={authUser ? <Home /> : <SignUp />}></Route>
         </Routes>
       </BrowserRouter>
 
-        <Toaster/>
+      <Toaster />
     </div>
   )
 }
