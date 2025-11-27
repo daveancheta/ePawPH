0a65a52a8cf5668b9a48489c452648503a2abd04
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-11-27 20:50:40 +0800

    feat: add following count endpoint to retrieve the number of users a logged-in user is following

--- backend/src/routes/FollowRoutes.route.js
+++ backend/src/routes/FollowRoutes.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { follow, getFollowing, unfollow } from "../controllers/follow.controller.js"
+import { follow, followingCount, getFollowing, unfollow } from "../controllers/follow.controller.js"
 import { protectRoute } from "../middleware/auth.middleware.js"
 
 const router = express.Router() 
@@ -7,5 +7,6 @@ const router = express.Router()
 router.post("/follow", follow)
 router.get("/following", protectRoute, getFollowing)
 router.post("/unfollow", protectRoute, unfollow)
+router.get("/followingCount", protectRoute, followingCount)
 
 export default router
