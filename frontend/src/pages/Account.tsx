eb0f56088e9276b00aff52281982c161c87ada8f
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-11-28 20:26:45 +0800

    feat: enhance following and follower display with improved UI and empty state messages

--- frontend/src/pages/Account.tsx
+++ frontend/src/pages/Account.tsx
@@ -4,7 +4,7 @@ import { useInitials } from "@/hooks/use-initials"
 import Layout from "@/layout/app-layout"
 import { UseAuthStore } from "@/store/UseAuthStore"
 import { UseFollowStore } from "@/store/UseFollowStore"
-import { Bolt, Bookmark, Copy, Heart, LayoutGrid, Search, Send } from "lucide-react"
+import { Bolt, Bookmark, Copy, Heart, LayoutGrid, Search, Send, Users2 } from "lucide-react"
 import { useEffect, useState } from "react"
 import {
     Dialog,
@@ -119,36 +119,56 @@ function Account() {
                                             <Search />
                                         </InputGroupAddon>
                                     </InputGroup>
+                                    {listFollowing.length > 0 ? <div className="space-y-6">
+                                        {
+                                            listFollowing.map((following: any) => (
+                                                <div>
+                                                    <div className="flex justify-between items-center">
+                                                        <div className="flex flex-row items-center gap-2">
+                                                            <Avatar key={following._id} className='w-12 h-12 rounded-full'>
+                                                                {following.followerId.profile.length > 0 ?
+                                                                    <img src={following.followerId.profile} alt="" /> :
+                                                                    <AvatarFallback className='text-white cursor-pointer border truncate whitespace-nowrap rounded-full'>
+                                                                        {getInitials(following.followerId.fullname)}
+                                                                    </AvatarFallback>}
+                                                            </Avatar>
+                                                            <div className="flex flex-col">
+                                                                <h1 className="font-medium capitalize">{following.followerId.fullname}</h1>
+                                                                <span className="text-muted-foreground">{following.followerId.username}</span>
+                                                            </div>
+                                                        </div>
 
-                                    {listFollowing.map((following: any) => (
-                                        <div>
-                                            <div className="flex justify-between items-center">
-                                                <div className="flex flex-row items-center gap-2">
-                                                    <Avatar key={following._id} className='w-12 h-12 rounded-full'>
-                                                        {following.followerId.profile.length > 0 ?
-                                                            <img src={following.followerId.profile} alt="" /> :
-                                                            <AvatarFallback className='text-white cursor-pointer border truncate whitespace-nowrap rounded-full'>
-                                                                {getInitials(following.followerId.fullname)}
-                                                            </AvatarFallback>}
-                                                    </Avatar>
-                                                    <div className="flex flex-col">
-                                                        <h1 className="font-medium capitalize">{following.followerId.fullname}</h1>
-                                                        <span className="text-muted-foreground">{following.followerId.username}</span>
+                                                        <div>
+                                                            {followings.some(f => f.followerId === following.followerId._id) ? <form onSubmit={handlesubmitUnFollow}>
+                                                                <Button variant={'following'} onClick={() => setFormData({ ...formData, followerId: following.followerId._id, followingId: following.followingId })}>Following</Button>
+                                                            </form> :
+                                                                <form onSubmit={handlesubmitFollow}>
+                                                                    <Button variant={'form'} onClick={() => setFormData({ ...formData, followerId: following.followerId._id, followingId: following.followingId })}>Follow</Button>
+                                                                </form>}
+
+                                                        </div>
                                                     </div>
                                                 </div>
-
-                                                <div>
-                                                    {followings.some(f => f.followerId === following.followerId._id) ? <form onSubmit={handlesubmitUnFollow}>
-                                                        <Button variant={'following'} onClick={() => setFormData({ ...formData, followerId: following.followerId._id, followingId: following.followingId })}>Following</Button>
-                                                    </form> :
-                                                        <form onSubmit={handlesubmitFollow}>
-                                                            <Button variant={'form'} onClick={() => setFormData({ ...formData, followerId: following.followerId._id, followingId: following.followingId })}>Follow</Button>
-                                                        </form>}
-
-                                                </div>
+                                            ))
+                                        }</div> :
+                                        <div className="flex flex-col items-center justify-center py-20 text-center">
+                                            <div className="p-4 bg-neutral-800 rounded-full">
+                                                <Users2 className="w-10 h-10 text-neutral-400" />
                                             </div>
-                                        </div>
-                                    ))}
+
+                                            <h2 className="mt-4 text-xl font-semibold text-white">
+                                                No Following Yet
+                                            </h2>
+
+                                            <p className="mt-2 text-neutral-400 max-w-sm">
+                                                When you follow someone, they will appear here.
+                                            </p>
+
+                                            <button className="mt-6 px-4 py-2 bg-blue-600 hover:bg-blue-700 rounded-lg text-white">
+                                                Discover People
+                                            </button>
+                                        </div>}
+
 
                                 </DialogContent>
                             </Dialog>
@@ -170,36 +190,52 @@ function Account() {
                                             <Search />
                                         </InputGroupAddon>
                                     </InputGroup>
+                                    {listFollower.length > 0 ? <div className="space-y-6">
+                                        {
+                                            listFollower.map((follower: any) => (
+                                                <div>
+                                                    <div className="flex justify-between items-center">
+                                                        <div className="flex flex-row items-center gap-2">
+                                                            <Avatar key={follower._id} className='w-12 h-12 rounded-full'>
+                                                                {follower.followingId.profile.length > 0 ?
+                                                                    <img src={follower.followingId.profile} alt="" /> :
+                                                                    <AvatarFallback className='text-white cursor-pointer border truncate whitespace-nowrap rounded-full'>
+                                                                        {getInitials(follower.followingId.fullname)}
+                                                                    </AvatarFallback>}
+                                                            </Avatar>
+                                                            <div className="flex flex-col">
+                                                                <h1 className="font-medium capitalize">{follower.followingId.fullname}</h1>
+                                                                <span className="text-muted-foreground">{follower.followingId.username}</span>
+                                                            </div>
+                                                        </div>
 
-                                    {listFollower.map((follower: any) => (
-                                        <div>
-                                            <div className="flex justify-between items-center">
-                                                <div className="flex flex-row items-center gap-2">
-                                                    <Avatar key={follower._id} className='w-12 h-12 rounded-full'>
-                                                        {follower.followingId.profile.length > 0 ?
-                                                            <img src={follower.followingId.profile} alt="" /> :
-                                                            <AvatarFallback className='text-white cursor-pointer border truncate whitespace-nowrap rounded-full'>
-                                                                {getInitials(follower.followingId.fullname)}
-                                                            </AvatarFallback>}
-                                                    </Avatar>
-                                                    <div className="flex flex-col">
-                                                        <h1 className="font-medium capitalize">{follower.followingId.fullname}</h1>
-                                                        <span className="text-muted-foreground">{follower.followingId.username}</span>
+                                                        <div>
+                                                            {followings.some(f => f.followerId === follower.followingId._id) ?
+                                                                <form onSubmit={handlesubmitUnFollow}>
+                                                                    <Button variant={'following'} onClick={() => setFormData({ ...formData, followerId: follower.followingId._id, followingId: follower.followerId })}>Following</Button>
+                                                                </form> :
+                                                                <form onSubmit={handlesubmitFollow}>
+                                                                    <Button variant={'form'} onClick={() => setFormData({ ...formData, followerId: follower.followingId._id, followingId: follower.followerId })}>Follow Back</Button>
+                                                                </form>}
+                                                        </div>
                                                     </div>
                                                 </div>
-
-                                                <div>
-                                                    {followings.some(f => f.followerId === follower.followingId._id) ?
-                                                        <form onSubmit={handlesubmitUnFollow}>
-                                                            <Button variant={'following'} onClick={() => setFormData({ ...formData, followerId: follower.followingId._id, followingId: follower.followerId })}>Following</Button>
-                                                        </form> :
-                                                        <form onSubmit={handlesubmitFollow}>
-                                                            <Button variant={'form'} onClick={() => setFormData({ ...formData, followerId: follower.followingId._id, followingId: follower.followerId })}>Follow Back</Button>
-                                                        </form>}
-                                                </div>
+                                            ))
+                                        }</div> :
+                                        <div className="flex flex-col items-center justify-center py-16 text-center">
+                                            <div className="p-4 bg-neutral-800 rounded-full">
+                                                <Users2 className="w-10 h-10 text-neutral-400" />
                                             </div>
-                                        </div>
-                                    ))}
+
+                                            <h2 className="mt-4 text-xl font-semibold text-white">
+                                                No Followers Yet
+                                            </h2>
+
+                                            <p className="mt-2 text-neutral-400 max-w-xs">
+                                                When someone follows you, they’ll appear here.
+                                            </p>
+                                        </div>}
+
                                 </DialogContent>
                             </Dialog>
                         </div>
