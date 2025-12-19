e4fca09b0dffa83a7e8fcb87dd596a55055ca9dd
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-12-19 23:19:14 +0800

    fix: api url

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,6 +2,6 @@
 import axios from "axios";
 
 export const AxiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : 'epawph-production.up.railway.app/api',
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : 'https://epawph-production.up.railway.app/api/',
     withCredentials: true,
 })
