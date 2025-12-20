f3e3995cea9470230609d35bd007d91f9ad9cd5f
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-12-20 22:54:34 +0800

    feat: open chat convo on mobile

--- frontend/src/layout/app-layout.tsx
+++ frontend/src/layout/app-layout.tsx
@@ -8,8 +8,8 @@ import { useInitials } from "@/hooks/use-initials"
 import { UseMessageStore } from "@/store/UseMessageStore"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-   const { auth } = UseAuthStore()
-   const { setChatContainer } = UseMessageStore()
+  const { auth } = UseAuthStore()
+  const { setChatContainer, selectedUser } = UseMessageStore()
   const getInitials = useInitials()
   return (
     <SidebarProvider>
@@ -23,42 +23,42 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <div className="flex flex-1 flex-col gap-4 pt-0 bg-neutral-900">
           <div className="flex rounded-xl justify-center min-h-min" >
             {children}
-             <div className="fixed bottom-0 left-0 w-full bg-neutral-900 border-t shadow-md z-50 md:hidden">
-                  <div className="flex items-center justify-between px-6 h-16">
-                    <Link to="/" className="flex items-center justify-center">
-                      <HomeIcon className="size-6 text-white" />
-                    </Link>
+            <div className={`fixed bottom-0 left-0 w-full bg-neutral-900 border-t shadow-md z-50 md:hidden ${selectedUser && "hidden"}`}>
+              <div className="flex items-center justify-between px-6 h-16">
+                <Link to="/" className="flex items-center justify-center">
+                  <HomeIcon className="size-6 text-white" />
+                </Link>
 
-                    <button onClick={() => setChatContainer("open")} className="flex items-center justify-center">
-                      <MessageCircleMore className="size-6 text-white" />
-                    </button>
+                <button onClick={() => setChatContainer("open")} className="flex items-center justify-center">
+                  <MessageCircleMore className="size-6 text-white" />
+                </button>
 
-                    <button className="">
-                      <Send className="size-6 text-white" />
-                    </button>
+                <button className="">
+                  <Send className="size-6 text-white" />
+                </button>
 
-                    <Link to="/news" className="flex items-center justify-center">
-                      <Globe className="size-6 text-white" />
-                    </Link>
+                <Link to="/news" className="flex items-center justify-center">
+                  <Globe className="size-6 text-white" />
+                </Link>
 
-                    <Link to="/account" className="flex items-center justify-center">
-                      <Avatar className="w-8 h-8">
-                        {auth.profile.length > 0 ? (
-                          <img
-                            className="rounded-full object-cover"
-                            src={auth.profile}
-                            alt="profile"
-                          />
-                        ) : (
-                          <AvatarFallback>
-                            {getInitials(auth.fullname)}
-                          </AvatarFallback>
-                        )}
-                      </Avatar>
-                    </Link>
+                <Link to="/account" className="flex items-center justify-center">
+                  <Avatar className="w-8 h-8">
+                    {auth.profile.length > 0 ? (
+                      <img
+                        className="rounded-full object-cover"
+                        src={auth.profile}
+                        alt="profile"
+                      />
+                    ) : (
+                      <AvatarFallback>
+                        {getInitials(auth.fullname)}
+                      </AvatarFallback>
+                    )}
+                  </Avatar>
+                </Link>
 
-                  </div>
-                </div>
+              </div>
+            </div>
           </div>
         </div>
       </SidebarInset>
