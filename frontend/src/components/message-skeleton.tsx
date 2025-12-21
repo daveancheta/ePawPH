92e4b45950dd4453446d5e2f785426a5e3231db0
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-12-21 10:13:12 +0800

    refactor: update message skeleton for mobile

--- frontend/src/components/message-skeleton.tsx
+++ frontend/src/components/message-skeleton.tsx
@@ -1,8 +1,23 @@
 import { Skeleton } from "@/components/ui/skeleton"
+import { useIsMobile } from "@/hooks/use-mobile"
 
 export function MessageSkeleton() {
+    const isMobile = useIsMobile()
     return (
-        <div className="flex-1 overflow-auto scrollbar-hide px-4 py-2 gap-1 flex flex-col">
+       <div className="flex-1 overflow-auto scrollbar-hide px-4 py-2 gap-1 flex flex-col">
+        {isMobile ?  <div>
+            {[1, 2].map((index) => (
+                <div key={index}>
+                    <div className="flex justify-start">
+                        <Skeleton className="h-12 w-50 rounded-xl" />
+                    </div>
+                    <div className="flex justify-end">
+                        <Skeleton className="h-12 w-50 rounded-xl" />
+                    </div>
+                </div>
+            ))}
+        </div> : 
+         <div>
             {[1, 2, 3, 4].map((index) => (
                 <div key={index}>
                     <div className="flex justify-start">
@@ -13,7 +28,8 @@ export function MessageSkeleton() {
                     </div>
                 </div>
             ))}
-        </div>
+        </div>}
+       </div>
 
 
     )
